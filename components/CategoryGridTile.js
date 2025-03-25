50732f6e90236b58e3d3fc31c6499071f84f17da
Three <ottunade95@gmail.com>
2025-03-25 13:20:26 +0100

    Add background color and improve UI components
    - Set background color in app.json
    - Add StatusBar component in App.js
    - Update dependencies in package.json for navigation
    - Enhance CategoryGridTile with ripple effect and shadow styling

--- components/CategoryGridTile.js
+++ components/CategoryGridTile.js
@@ -1,16 +1,23 @@
-import { Text, View, Pressable, StyleSheet } from "react-native";
+import { Text, View, Pressable, StyleSheet, Platform } from "react-native";
 
 function CategoryGridTile({ title, color }) {
   return (
     <View style={styles.gridItem}>
-      <Pressable style={styles.button}>
-        <View style={styles.innerContainer}>
-          <Text>{title}</Text>
+      <Pressable
+        android_ripple={{ color: "#ccc" }}
+        style={({ pressed }) => [
+          styles.button,
+          pressed ? styles.buttonPressed : null,
+        ]}
+      >
+        <View style={[styles.innerContainer, { backgroundColor: color }]}>
+          <Text style={styles.title}>{title}</Text>
         </View>
       </Pressable>
     </View>
   );
 }
+// in the inner view style we put the background color there because of the shadow effect.
 
 export default CategoryGridTile;
 
@@ -21,14 +28,29 @@ const styles = StyleSheet.create({
     height: 150,
     borderRadius: 8,
     elevation: 4,
+    shadowColor: "black",
+    overflow: Platform.OS === "android" ? "hidden" : "visible",
+    // for ios
+    // shadowOpacity: 0.25,
+    //  shadowOffset: { width: 0, height: 2 },
+    //   shadowRadius: 8,
+    //  backgroundColor: "white",
   },
   button: {
     flex: 1,
   },
   innerContainer: {
     flex: 1,
+    // borderRadius: 8, // for IOS due to the corners not being rounded
     padding: 16,
     alignItems: "center",
     justifyContent: "center",
+  },
+  buttonPressed: {
+    opacity: 0.5, // this is for ios
+  },
+  title: {
+    fontSize: 17,
+    fontWeight: "bold",
   },
 });
