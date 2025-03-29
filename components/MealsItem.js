6e3e758f5877dc8b866c99929696a8dbffae884c
Three <ottunade95@gmail.com>
2025-03-29 14:53:45 +0100

    Enhance MealsItem component to include meal details and update MealsOverviewScreen to pass additional props

--- components/MealsItem.js
+++ components/MealsItem.js
@@ -1,11 +1,27 @@
-import { View, Text, Pressable, Image, StyleSheet } from "react-native";
-function MealsItem({ title, imageUrl }) {
+import {
+  View,
+  Text,
+  Pressable,
+  Image,
+  StyleSheet,
+  Platform,
+} from "react-native";
+function MealsItem({ title, imageUrl, duration, complexity, affordability }) {
   return (
-    <View>
+    <View style={styles.mealItem}>
       <Pressable>
-        <View>
-          <Image source={{ uri: imageUrl }} style={styles.image} />
-          <Text style={styles.title}>{title}</Text>
+        <View style={styles.innerContainer}>
+          <View>
+            <Image source={{ uri: imageUrl }} style={styles.image} />
+            <Text style={styles.title}>{title}</Text>
+          </View>
+          <View style={styles.details}>
+            <Text style={styles.detailsItem}>{duration}m</Text>
+            <Text style={styles.detailsItem}>{complexity.toUpperCase()}</Text>
+            <Text style={styles.detailsItem}>
+              {affordability.toUpperCase()}
+            </Text>
+          </View>
         </View>
       </Pressable>
     </View>
@@ -18,6 +34,18 @@ function MealsItem({ title, imageUrl }) {
 export default MealsItem;
 
 const styles = StyleSheet.create({
+  mealItem: {
+    margin: 16,
+    borderRadius: 8,
+    overflow: "hidden",
+    backgroundColor: "white",
+    elevation: 4,
+    // overflow: Platform.OS === "android" ? "hidden" : "visible",
+    // for ios
+    // shadowOpacity: 0.25,
+    //  shadowOffset: { width: 0, height: 2 },
+    //   shadowRadius: 8,
+  },
   image: {
     width: "100%",
     height: 200,
@@ -26,5 +54,21 @@ const styles = StyleSheet.create({
     fontSize: 18,
     fontWeight: "bold",
     textAlign: "center",
+    margin: 8,
+  },
+  details: {
+    flexDirection: "row",
+    alignItems: "center",
+    padding: 8,
+    justifyContent: "center",
+  },
+  innerContainer: {
+    // this is the container that holds the image and the text and the rounded corners disappeard on ios due to the overflow hidden
+    borderRadius: 8,
+    overflow: "hidden",
+  },
+  detailsItem: {
+    marginHorizontal: 4,
+    fontSize: 12,
   },
 });
--- screens/MealsOverviewScreen.js
+++ screens/MealsOverviewScreen.js
@@ -5,14 +5,22 @@ import { MEALS } from "../data/dummy-data";
 function MealsOverviewScreen({ route }) {
   const catID = route.params.categoryId; // we are showing the data passed from the CategoriesScreen
 
+  // we are filtering the meals based on the category id which is stored in the mealItem object.
   const displayedMeals = MEALS.filter((mealItem) => {
     return mealItem.categoryIds.indexOf(catID) >= 0;
   }); // we start by storing the data we want to display in a const, we are then filtering
   // the meals based on the category id. we then wan to return true if the category id is included in the categoryIds array of the mealItem.
   function renderMealItem(itemData) {
-    return (
-      <MealsItem title={itemData.item.title} imageUrl={itemData.item.imageUrl} />
-    );
+    const item = itemData.item;
+    const mealProps = {
+      title: item.title,
+      imageUrl: item.imageUrl,
+      duration: item.duration,
+      complexity: item.complexity,
+      affordability: item.affordability,
+    };
+
+    return <MealsItem {...mealProps} />;
   }
   return (
     <View>
