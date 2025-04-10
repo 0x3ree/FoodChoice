7534061e1329e2b73a3e4fa053f0274d9101ef84
Three <ottunade95@gmail.com>
2025-04-10 14:02:59 +0100

    Add IconButton component and implement header button in MealDetailScreen for enhanced navigation

--- screens/MealDetailScreen.js
+++ screens/MealDetailScreen.js
@@ -1,14 +1,33 @@
 import { Text, View, Image, StyleSheet, ScrollView } from "react-native";
 import { MEALS } from "../data/dummy-data";
+import { useLayoutEffect } from "react";
 import MealDetails from "../components/MealDetails";
 import Subtitle from "../components/MealDetails/Subtitle";
+import IconButton from "../components/IconButton";
 import List from "../components/MealDetails/List";
 
-function MealDetailScreen({ route }) {
+function MealDetailScreen({ route, navigation }) {
   const mealId = route.params.mealId; // it enables the mealdetailsscreen to dynamically display content based on the meal id passed from the mealsOverview screen. we can then use this mealId to fetch the data for that meal item from the MEALS array in the dummy-data.js file. we can also use it to navigate to other screens or to get the navigation prop in a functional component. we can also use it to get the route prop in a functional component.
   // (s)we can also use the useNavigation hook to get the navigation prop in a functional component, but we are using it in the MealsItem component to navigate to the MealDetail screen. we can also use it to get the route prop in a functional component, but we are using it in the MealsOverview screen to get the categoryId passed from the CategoriesScreen.
   // /s/ we can also use it to navigate to other screens or to get the navigation prop in a functional component, but we are using it in the MealsItem component to navigate to the MealDetail screen. we can also use it to get the route prop in a functional component, but we are using it in the MealsOverview screen to get the categoryId passed from the CategoriesScreen.
   const selectedMeal = MEALS.find((meal) => meal.id === mealId);
+  function HeaderButtonPressHandler() {
+    console.log("Pressed!");
+  }
+
+  useLayoutEffect(() => {
+    navigation.setOptions({
+      headerRight: () => {
+        return (
+          <IconButton
+            icon="star"
+            color="white"
+            onPress={HeaderButtonPressHandler}
+          />
+        );
+      },
+    });
+  }, [navigation]);
   return (
     <ScrollView style={styles.rootContainer}>
       <Image style={styles.image} source={{ uri: selectedMeal.imageUrl }} />
